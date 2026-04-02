ad32c72ee9608bf8adea58c2c9eb8c996abdb083
Pyxisplash <poppylillyshanti@gmail.com>
2026-04-02 16:24:49 +0100

    HIDEEEEEE
    HIDEEEE!!

--- Assets/Scripts/Themis's scripts/PlayerHide.cs
+++ Assets/Scripts/Themis's scripts/PlayerHide.cs
@@ -2,21 +2,47 @@ using UnityEngine;
 
 public class PlayerHide : MonoBehaviour
 {
-    [Header("Camera Position")]
+    [Header("Camera Positions")]
     public Transform normalPosition;
     public Transform hidePosition;
 
     [Header("Settings")]
-    public float movespped = 5f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f;
+
+    public bool isHiding = false;
+
+    private Camera mainCam;
+
     void Start()
     {
-        
+        mainCam = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MoveCamera();
+    }
+
+    public void ToggleHide()
+    {
+        isHiding = !isHiding;
+        Debug.Log("Hiding: " + isHiding);
+    }
+
+    void MoveCamera()
+    {
+        Transform target = isHiding ? hidePosition : normalPosition;
+
+        mainCam.transform.position = Vector3.Lerp(
+            mainCam.transform.position,
+            target.position,
+            Time.deltaTime * moveSpeed
+        );
+
+        mainCam.transform.rotation = Quaternion.Lerp(
+            mainCam.transform.rotation,
+            target.rotation,
+            Time.deltaTime * moveSpeed
+        );
     }
 }
